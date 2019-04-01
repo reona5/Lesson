331ef2fcec8cb470cf9389257a7b0eaeef2b5491
reona5 <leonardooooo55555@gmail.com>
2019-04-01 16:36:46 +0900

    Create post model

---
+++ db/migrate/20190401065811_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :name, limit: 30, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
