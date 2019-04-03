fe9475c1c655ebb0302d2e6b1e4ac030f6f850c1
reona5 <leonardooooo55555@gmail.com>
2019-04-04 00:26:04 +0900

    modify devise for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {host: 'http://アプリ名.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => "app129345791@heroku.com",
+      :password => "688dn2m26221",
+      :domain => "heroku.com",
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
