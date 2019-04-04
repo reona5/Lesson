747b5a30bb353384d0416b1a083227cd26c6460d
reona5 <leonardooooo55555@gmail.com>
2019-04-04 14:10:32 +0900

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,16 +92,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'http://アプリ名.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  config.action_mailer.default_url_options = {host: 'lesson-code.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => "app129345791@heroku.com",
-      :password => "688dn2m26221",
-      :domain => "heroku.com",
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => "herokuapp.com",
       :address => "smtp.sendgrid.net",
       :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true
   }
+end
 end
