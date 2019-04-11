909cdfb3b94ffbe2c2ab19539c598a7d477f9e7b
reona5 <leonardooooo55555@gmail.com>
2019-04-11 12:41:19 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
-  root 'pages#index'
-  get 'pages/show'
-  get 'pages/index'
-  root 'posts#index'
-  get 'posts/index'
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  root to: 'pages#index'
+
+  resources :pages, only: [:index, :show]
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
