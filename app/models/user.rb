fbe789aeb5d7aa1307a0b80a34c5045f8ef71f91
reona5 <leonardooooo55555@gmail.com>
2019-04-11 13:34:10 +0900

    validate remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -6,20 +8,25 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
 
   def self.from_omniauth(auth)
-    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.username = auth["info"]["nickname"]
+    find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.username = auth['info']['nickname']
     end
   end
 
   def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"]) do |user|
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes']) do |user|
         user.attributes = params
       end
     else
       super
     end
+  end
+
+  # remember_me機能有効化
+  def remember_me
+    true
   end
 end
