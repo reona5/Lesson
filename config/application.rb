00aecb98f3424cf3cba6f9bfb4543bb65207690a
reona5 <leonardooooo55555@gmail.com>
2019-04-12 20:32:30 +0900

    Revert "remove unuse gem"
    This reverts commit 4f05c22079318ff83b50ab9719e070ebcdc35f23.

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails/all'
-require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
